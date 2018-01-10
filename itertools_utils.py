9c4e57450287b9f60ba8992f7ffac6491a76ce01
dimathe47 <dimathe47@yandex.ru>
2018-01-10 10:51:16 +0300

    commit with bug: cant compute cnn activations descriptors restore keras_utils

--- itertools_utils.py
+++ itertools_utils.py
@@ -16,7 +16,22 @@ def chunkify(iterable_, chunk_size, numpy_array_chunks=True):
         # for i in it_iter:
         #     print(i.shape)
         chunks_stream = tw(bool, (tuple(islice(it_iter, chunk_size)) for _ in count()))
+    # chunks_stream = tee_print_iterable(chunks_stream)
+    for chunk_stream in chunks_stream:
+        print(chunk_stream)
+        print(np.array(chunk_stream,copy=False).shape)
 
     if numpy_array_chunks:
         chunks_stream = map(partial(np.array, copy=False), chunks_stream)
+    # chunks_stream = tee_print_iterable(chunks_stream)
     return chunks_stream
+
+
+def tee_print_iterable(iterable_, shape_only=True):
+    iterable_, iterable_copy = itertools.tee(iterable_)
+    for i in iterable_copy:
+        if isinstance(i, np.ndarray) and shape_only:
+            print(i.shape)
+        else:
+            print(i)
+    return iterable_
